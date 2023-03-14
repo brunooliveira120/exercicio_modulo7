260cc6089b4cfda75e60ce5049e009362c98f96f
Bruno Oliveira <brunoo.oliveira120@gmail.com>
2023-03-14 12:16:34 -0300

    confirmador

--- main.js
+++ main.js
@@ -8,13 +8,12 @@ form.addEventListener('submit', (e) =>{
 
 function confirmador(valorA,valorB){
     if(valorA < valorB){
-        alert("Valor A=", + valorA, " é menor que valor B= ", + valorB)
+        alert("Valor A= ", + valorA, " é menor que valor B= ", + valorB)
     }else if(valorA = valorB){
-        alert("Valor A=", + valorA, " é igual que valor B= ", + valorB)
+        alert("Valor A= ", + valorA, " é igual que valor B= ", + valorB)
     }else if(valorA > valorB){
-        alert("Valor A=", + valorA, " é maior que valor B= ", + valorB)
+        alert("Valor A= ", + valorA, " é maior que valor B= ", + valorB)
     }
-    return confirmador
 }
 
-console.log(form);
+console.log(confirmador);

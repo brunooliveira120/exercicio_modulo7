bd353ad3dcfa14c747571e1779c9b6738ae819dd
Bruno Oliveira <brunoo.oliveira120@gmail.com>
2023-03-14 11:52:53 -0300

    alterações

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const form = document.getElementById('form-numero');
 
+function confirmador(valorA,valorB){
+    if(valorA <= valorB){
+        alert("Valor A=" + valorA, " é menor que valor B= " + valorB)
+    }else{
+        alert("Valor A=" + valorA, " é maior que valor B= " + valorB)
+    }
+}
+
 form.addEventListener('submit', (e) =>{
     let formEvalido = false;
     e.preventDefault();
@@ -7,15 +15,16 @@ form.addEventListener('submit', (e) =>{
 
     const valorA = document.getElementById('valorA');
     const valorB = document.getElementById('valorB');
-    }
-)
+    const messagem = `Valor A=${valorA} é menor que o valor B=${valorB}`
 
-function confirmador(valorA,valorB){
-    if(valorA <= valorB){
-        alert("Valor A= " + valorA, " é menor que valor B= " + valorB)
-    }else{
-        alert("Valor A= " + valorA, " é maior que valor B= " + valorB)
-    }
-    return confirmador
-}
+    formEvalido = confirmador
+        if(confirmador){
+            alert(messagem)
+        }else{
+            alert("valor invalido")
+        }
+    
+        return numerovalido;
+    })
+
 console.log(form);

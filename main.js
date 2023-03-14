b6252332a042937ea65efda1c85695bf3b6eed22
Bruno Oliveira <brunoo.oliveira120@gmail.com>
2023-03-14 12:13:45 -0300

    else

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function confirmador(valorA,valorB){
         alert("Valor A=", + valorA, " é menor que valor B= ", + valorB)
     }else if(valorA == valorB){
         alert("Valor A=", + valorA, " é igual que valor B= ", + valorB)
-    }else if(valorA > valorB){
+    }else(valorA > valorB){
         alert("Valor A=", + valorA, " é maior que valor B= ", + valorB)
     }
     return confirmador
